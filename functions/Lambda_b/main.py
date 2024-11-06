254176b19436404d014dfc483c100e5ef89810f8
Leonardo Abreu <leonardoaall125@gmail.com>
2024-11-05 21:56:58 -0300

    testing reading files in the function dir

--- functions/Lambda_b/main.py
+++ functions/Lambda_b/main.py
@@ -1,8 +1,19 @@
-def lambda_handler(event, context):
-    print("Hello, this is a simple print statement in a Lambda function!")
+import os
 
+def lambda_handler(event, context):
+    cwd = os.path.dirname(__file__)
+    file_path = os.path.join(cwd, "test.txt")
+    
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            print("File content:", content)
+    except Exception as e:
+        print("Error reading file:", e)
     # You can return a response if needed
     return {
         'statusCode': 200,
         'body': 'Check CloudWatch logs for the printed message.'
     }
+    
+    
